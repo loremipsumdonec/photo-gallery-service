e9d63f96c5183ed709d88fa8d1895848651cc163
loremipsumdonec <mikael.karlsson@tiff.se>
2022-02-04 15:11:55 +0100

    Updated subscriptions

--- src/PhotoGalleryService/Features/Gallery/Schema/GallerySubscription.cs
+++ src/PhotoGalleryService/Features/Gallery/Schema/GallerySubscription.cs
@@ -6,5 +6,17 @@ namespace PhotoGalleryService.Features.Gallery.Schema
     {
         [Subscribe]
         public ImageCreated OnImageCreated([EventMessage] ImageCreated @event) => @event;
+
+        [Subscribe]
+        public ImageDeleted OnImageDeleted([EventMessage] ImageDeleted @event) => @event;
+
+        [Subscribe]
+        public ImageFileUploaded OnImageFileUploaded([EventMessage] ImageFileUploaded @event) => @event;
+
+        [Subscribe]
+        public ImageFileDeleted OnImageFileDeleted([EventMessage] ImageFileDeleted @event) => @event;
+
+        [Subscribe]
+        public ImageUpdated OnImageUpdated([EventMessage] ImageUpdated @event) => @event;
     }
 }
