14537f79e78ea0e0cb5266deb456b9ed7927d6bc
loremipsumdonec <mikael.karlsson@tiff.se>
2022-02-07 21:04:43 +0100

    Added Elastic APM for commands

---
+++ src/Boilerplate/Features/ElasticApm/Commands/ElasticApmCommandDispatcherDecorator.cs
@@ -0,0 +1,48 @@
+﻿using Boilerplate.Features.Core.Commands;
+using Elastic.Apm.Api;
+
+namespace Boilerplate.Features.ElasticApm.Commands
+{
+    public class ElasticApmCommandDispatcherDecorator
+        : ICommandDispatcher
+    {
+        private readonly ICommandDispatcher _decorated;
+        private readonly ITracer _tracer;
+
+        public ElasticApmCommandDispatcherDecorator(ICommandDispatcher decorated)
+        {
+            _decorated = decorated;
+        }
+
+        public async Task<bool> DispatchAsync(ICommand command)
+        {
+            ISpan span = null;
+
+            if (_tracer.CurrentTransaction != null)
+            {
+                span = _tracer.CurrentTransaction.StartSpan(command.GetType().Name, "command");
+            }
+
+            try
+            {
+                return await _decorated.DispatchAsync(command);
+            }
+            catch (Exception ex)
+            {
+                if (span != null)
+                {
+                    span.CaptureException(ex);
+                }
+
+                throw;
+            }
+            finally
+            {
+                if (span != null)
+                {
+                    span.End();
+                }
+            }
+        }
+    }
+}
---
+++ src/Boilerplate/Features/ElasticApm/Commands/ElasticApmTransactionCommandDispatcherDecorator.cs
@@ -0,0 +1,49 @@
+﻿using Boilerplate.Features.Core.Commands;
+using Elastic.Apm.Api;
+
+namespace Boilerplate.Features.ElasticApm.Commands
+{
+    public class ElasticApmTransactionCommandDispatcherDecorator
+        : ICommandDispatcher
+    {
+        private readonly ICommandDispatcher _decorated;
+        private readonly ITracer _tracer;
+
+        public ElasticApmTransactionCommandDispatcherDecorator(ICommandDispatcher decorated, ITracer tracer)
+        {
+            _decorated = decorated;
+            _tracer = tracer;
+        }
+
+        public async Task<bool> DispatchAsync(ICommand command)
+        {
+            ITransaction transaction = null;
+
+            if (_tracer.CurrentTransaction == null)
+            {
+                transaction = _tracer.StartTransaction(command.GetType().Name, "command");
+            }
+
+            try
+            {
+                return await _decorated.DispatchAsync(command);
+            }
+            catch (Exception ex)
+            {
+                if (transaction != null)
+                {
+                    transaction.CaptureException(ex);
+                }
+
+                throw;
+            }
+            finally
+            {
+                if (transaction != null)
+                {
+                    transaction.End();
+                }
+            }
+        }
+    }
+}
