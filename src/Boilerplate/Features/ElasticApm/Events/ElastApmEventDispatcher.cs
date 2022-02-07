7e6c60e75cfd998337779c366f4fd86a4eab769e
loremipsumdonec <mikael.karlsson@tiff.se>
2022-02-07 21:48:56 +0100

    Added Elastic APM for events

---
+++ src/Boilerplate/Features/ElasticApm/Events/ElastApmEventDispatcher.cs
@@ -0,0 +1,50 @@
+﻿using Boilerplate.Features.Reactive.Events;
+using Boilerplate.Features.Reactive.Services;
+using Elastic.Apm.Api;
+
+namespace Boilerplate.Features.ElasticApm.Events
+{
+    public class ElastApmEventDispatcher
+        : IEventDispatcher
+    {
+        private readonly IEventDispatcher _decorated;
+        private readonly ITracer _tracer;
+
+        public ElastApmEventDispatcher(IEventDispatcher decorated, ITracer tracer)
+        {
+            _decorated = decorated;
+            _tracer = tracer;
+        }
+
+        public void Dispatch(IEvent @event)
+        {
+            ISpan span = null;
+
+            if (_tracer.CurrentTransaction != null)
+            {
+                span = _tracer.CurrentTransaction.StartSpan(@event.GetType().Name, "event");
+            }
+
+            try
+            {
+                _decorated.Dispatch(@event);
+            }
+            catch (Exception ex)
+            {
+                if (span != null)
+                {
+                    span.CaptureException(ex);
+                }
+
+                throw;
+            }
+            finally
+            {
+                if (span != null)
+                {
+                    span.End();
+                }
+            }
+        }
+    }
+}
